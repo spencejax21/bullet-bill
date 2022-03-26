a3cf7ac352486bb8ceaf5965c1c73f93eb64bef4
Kevin Lee <123kevinlee@gmail.com>
2022-03-26 12:56:02 -0400

    setup

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const multer  = require('multer')
+
 const app = express()
 const port = 3000
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+var upload = multer({ storage: storage })
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
