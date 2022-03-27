4cd3f824a8744005b32e44a50069b90f2e2eafc6
Kevin Lee <123kevinlee@gmail.com>
2022-03-26 22:14:55 -0400

    gcs uploading

--- app.js
+++ app.js
@@ -1,40 +1,48 @@
-var express = require('express')
-var multer  = require('multer')
+var express = require('express');
+var Multer  = require('multer');
 var path = require('path');
 var id = require('shortid');
 const send_receipt = require('./receipt');
 
-var port = 3000;
-
-var app = express()
-
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, `${id.generate()}.png`);
-    }
-})
-var upload = multer({ storage: storage })
-
-app.use('/uploads', express.static('uploads'));
+var app = express();
 app.use(express.static('pages/'));
+
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 25 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+  },
+});
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
 app.get('/', (req, res) => {
     res.render(path.join(__dirname, "/pages/home.ejs"));
 })
 
-app.post('/receipt-upload', upload.single('profile-file'), function (req, res, next) {
-  // req.file is the `profile-file` file
-  // req.body will hold the text fields, if there were any
-  //console.log(path.join(__dirname, `/uploads/${req.file.filename}`));
-  send_receipt(path.join(__dirname, `/uploads/${req.file.filename}`));
-  console.log(JSON.stringify(req.file))
-  var response = '<a href="/">Home</a><br>'
-  response += "Files uploaded successfully.<br>"
-  response += `<img src="${req.file.path}" /><br>`
-  return res.send(response)
-})
+app.post('/receipt-upload', multer.single('profile-file'), (req, res, next) => {
+  if (!req.file) {
+    res.status(400).send('No file uploaded.');
+    return;
+  }
+
+  const blob = bucket.file(`${id.generate()}.png`);
+  const blobStream = blob.createWriteStream();
+
+  blobStream.on('error', err => {
+    next(err);
+  });
+
+  blobStream.on('finish', () => {
+    var parsed = send_receipt(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
+    parsed.then(function(result){
+      return res.send(result);
+    })
+  });
+
+  blobStream.end(req.file.buffer);
+});
   
-app.listen(port,() => console.log(`Server running on port ${port}!`))
+const port = process.env.PORT || 3000;
+app.listen(port,() => console.log(`Server running on port ${port}!`));
