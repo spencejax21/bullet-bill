132af69f95615db2a7b516f058d4e2d7ab600909
Kevin Lee <123kevinlee@gmail.com>
2022-03-27 04:34:18 -0400

    last function

--- pages/script.js
+++ pages/script.js
@@ -15,5 +15,16 @@ document.addEventListener("DOMContentLoaded", function(){
         this.innerHTML = "Processing...";
         this.className = "upload disabled";
     }
+
+    var textsend = document.getElementById("textsend");
+    textsend.onclick = function(){
+        var x = document.getElementById("myTable").rows.length;
+        for(var i = 1; i<x; i++){
+            document.getElementById("table").rows[i].cells[0].innerText;
+            document.getElementById("table").rows[i].cells[1].innerText;
+        }
+    }
 });
+
+
 
